4bbe92ea70ad63a98a07fd717acb7dbc520b83f8
Levi Payne <levi.dyrek@gmail.com>
2019-10-19 17:05:57 -0400

    Improve overall styling
    * Replaced React logos with EOSIO logo
    * Replaced React default descriptions and titles
    * Updated site-wide styling of fonts and buttons
    * Added logo to next to the page title
    * Updated App component snapshots

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import RecentBlocks from './containers/RecentBlocks';
+import logo from './logo.png';
+import './stylesheets/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>EOSIO Recent Blocks</h1>
+      <img className="logo" src={logo} alt="EOSIO logo" />
+      <h1 className="title">EOSIO Recent Blocks</h1>
       <RecentBlocks />
     </div>
   );
